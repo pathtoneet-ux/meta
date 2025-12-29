4ad4c2c73405ef9bff61dabfd06a226994353e86
Ritik <pathtoneet@gmail.com>
2025-12-29 19:23:08 +0530

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,3 +22,23 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+
+
+/*ab yaha hai */
+
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
